b522fcb4f50cb03cb66cd292bb7e2a917662de98
Sakti Abdullah Shidiq <specialhisaki@gmail.com>
2023-10-08 14:00:04 +0700

    first time using vue, wish me luck

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header>
+            <div>
+                <h1>Mamat Gun Shop</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Catalog</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+            <div>
+                <button>Log In</button>
+            </div>
+        </header>
+    </div>
+</template>
